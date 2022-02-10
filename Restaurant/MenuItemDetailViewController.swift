d53dc6242f50f71cbfe6760392b796321fddf0b7
Knarik <knarik.kloyan@gmail.com>
2022-02-10 15:23:40 +0400

    Add Menu Items

--- Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/MenuItemDetailViewController.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+@MainActor
 class MenuItemDetailViewController: UIViewController {
     
+    let menuItem: MenuItem
+    
+    init?(coder: NSCoder, menuItem: MenuItem) {
+        self.menuItem = menuItem
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
