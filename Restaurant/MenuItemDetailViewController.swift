39eb24728984826678e0c288e115daf94429d164
Knarik <knarik.kloyan@gmail.com>
2022-02-14 15:46:09 +0400

    Add Menu Details

--- Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/MenuItemDetailViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 @MainActor
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var detailTextLabel: UILabel!
+    @IBOutlet var addToOrderButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
+    }
+    
+    @IBAction func addToOrderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: [], animations: {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
+    
     let menuItem: MenuItem
     
     init?(coder: NSCoder, menuItem: MenuItem) {
